124e21049b1e9506342c387bcc43a8cb6aaf2843
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-08-07 22:02:37 +1000

    Getting gorund work done

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import getArtistList from "./lib/GetArtistList";
+
+const artists = getArtistList("NOFX");
+
+console.log(artists);
---
+++ src/lib/GetArtistList.ts
@@ -0,0 +1,29 @@
+import * as https from "https";
+
+export default function(artistName : string) {
+    const baseArtistSearchUrl = "https://musicbrainz.org/ws/2/artist?query=";
+    const userAgent = "Musichino_Bot/0.1 ( https://github.com/vitorussomoliterni/musichino/ )";
+    var rawData = '';
+
+    const options = {
+        hostname: "musicbrainz.org",
+        path: "/ws/2/artist?query=" + artistName,
+        method: "GET",
+        headers: {
+            "User-Agent": userAgent
+        }
+    };
+    
+    https.get(options, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', ()=> {
+            console.log(rawData); 
+        })
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
+
+    return rawData; 
+}
