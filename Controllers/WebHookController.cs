a332404e63647c59712c611c63ecdd3b1049f0fd
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-08-12 11:50:02 +1000

    Trying to pass more info from the service to the controller in an appropriate way, failing to navigate the XML response easily

--- Controllers/WebHookController.cs
+++ Controllers/WebHookController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using musichino.Services;
+using musichino.Commands;
 
 namespace musichino.Controllers
 {
@@ -22,17 +23,15 @@ namespace musichino.Controllers
         [HttpPost("")]
         public async Task<IActionResult> Receive()
         {
-
             using (var reader = new StreamReader(Request.Body))
             {
                 var messageResponse = String.Empty;
                 var text = await reader.ReadToEndAsync();
-
                 var response = await _musicbrainz.GetArtistNameList(text);
 
-                foreach (var item in response)
+                foreach (var artist in response)
                 {
-                    messageResponse += item + "\n";
+                    messageResponse += artist.Name + "\n";
                 }
 
                 return Ok(messageResponse);
--- Services/MusicbrainzService.cs
+++ Services/MusicbrainzService.cs
@@ -3,60 +3,78 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
+using musichino.Commands;
 
 namespace musichino.Services
 {
-public class MusicbrainzService
-{
-    public async Task<IEnumerable<string>> GetArtistNameList(string artistName)
+    public class MusicbrainzService
     {
-        using (var client = new HttpClient())
+        public async Task<IEnumerable<ArtistCommand>> GetArtistNameList(string artistName)
         {
-            try
+            using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("https://musicbrainz.org");
-                client.DefaultRequestHeaders.Add("User-Agent", "Musichino_Bot/0.1 ( https://github.com/vitorussomoliterni/musichino/ )");
+                try
+                {
+                    client.BaseAddress = new Uri("https://musicbrainz.org");
+                    client.DefaultRequestHeaders.Add("User-Agent", "Musichino_Bot/0.1 ( https://github.com/vitorussomoliterni/musichino/ )");
 
-                // var response = await client.GetAsync("/ws/2/artist?query=" + artistName);
-                
-                // response.EnsureSuccessStatusCode();
+                    // var response = await client.GetAsync("/ws/2/artist?query=" + artistName);
+                    
+                    // response.EnsureSuccessStatusCode();
 
-                // var stringResponse = await response.Content.ReadAsStringAsync();
+                    // var stringResponse = await response.Content.ReadAsStringAsync();
 
-                var stringResponse = ReadXmlResponse(GetRawdataForTesting());
+                    var artists = ReadXmlResponse(GetRawdataForTesting());
 
-                return stringResponse;
+                    return artists;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return null;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            return null;
-        }
-    }
-
-    private IEnumerable<string> ReadXmlResponse(string body)
-    {
-        var doc = new XmlDocument();
-        doc.LoadXml(body);
-
-        var artists = new List<string>();
-
-        var nodes = doc.DocumentElement.GetElementsByTagName("artist");
-
-        foreach (XmlNode a in nodes)
-        {
-            var artistName = a["name"].InnerText;
-            artists.Add(artistName);
         }
 
-        return artists;
-    }
+        private IEnumerable<ArtistCommand> ReadXmlResponse(string body)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(body);
 
-    private string GetRawdataForTesting()
-    {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><metadata created=\"2017-08-08T11:32:33.317Z\" xmlns=\"http://musicbrainz.org/ns/mmd-2.0#\" xmlns:ext=\"http://musicbrainz.org/ns/ext#-2.0\"><artist-list count=\"2\" offset=\"0\"><artist id=\"dcaa4f81-bfb7-44eb-8594-4e74f004b6e4\" type=\"Group\" ext:score=\"100\"><name>NOFX</name><sort-name>NOFX</sort-name><country>US</country><area id=\"489ce91b-6658-3307-9877-795b68554c98\"><name>United States</name><sort-name>United States</sort-name></area><begin-area id=\"1f40c6e1-47ba-4e35-996f-fe6ee5840e62\"><name>Los Angeles</name><sort-name>Los Angeles</sort-name></begin-area><life-span><begin>1983</begin><ended>false</ended></life-span><alias-list><alias sort-name=\"No FX\">No FX</alias></alias-list><tag-list><tag count=\"1\"><name>rock</name></tag><tag count=\"3\"><name>punk</name></tag><tag count=\"3\"><name>american</name></tag><tag count=\"2\"><name>punk rock</name></tag><tag count=\"1\"><name>ska punk</name></tag><tag count=\"2\"><name>usa</name></tag><tag count=\"1\"><name>hardcore punk</name></tag><tag count=\"1\"><name>américain</name></tag><tag count=\"1\"><name>pop punk</name></tag><tag count=\"2\"><name>skate punk</name></tag><tag count=\"1\"><name>los angeles</name></tag><tag count=\"1\"><name>bay area</name></tag><tag count=\"1\"><name>classic pop punk</name></tag><tag count=\"1\"><name>california punk</name></tag><tag count=\"1\"><name>classic hardcore punk</name></tag><tag count=\"1\"><name>classic punk</name></tag><tag count=\"1\"><name>los angeles punk</name></tag><tag count=\"1\"><name>bay area punk</name></tag></tag-list></artist><artist id=\"8ab0d990-40d2-4625-835d-c68a16295ee0\" type=\"Person\" ext:score=\"72\"><name>DJ NoFx</name><sort-name>NoFx, DJ</sort-name><gender>male</gender><disambiguation>dance music DJ/producer Patrick Sanders</disambiguation><life-span><ended>false</ended></life-span><alias-list><alias sort-name=\"Sanders, Patrick\">Patrick Sanders</alias></alias-list></artist></artist-list></metadata>";
+            var artists = new List<ArtistCommand>();
+
+            var nodes = doc.DocumentElement.GetElementsByTagName("artist");
+
+            foreach (XmlNode a in nodes)
+            {
+                var artist = new ArtistCommand();
+
+                // artist.Id = a["id"].InnerText;
+                // artist.Type = a["type"].InnerText;
+                artist.Name = a["name"].InnerText;
+                artist.Country = a["country"].InnerText;
+                // artist.BeginYear = a["life-span"].InnerText;
+
+                // if (a["life-span"].InnerText.Equals("false"))
+                // {
+                //     artist.Ended = false;
+                //     artist.EndYear = null;
+                // }
+                // else
+                // {
+                //     artist.Ended = true;
+                // }
+
+                artists.Add(artist);
+            }
+
+            return artists;
+        }
+
+        private string GetRawdataForTesting()
+        {
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><metadata created=\"2017-08-08T11:32:33.317Z\" xmlns=\"http://musicbrainz.org/ns/mmd-2.0#\" xmlns:ext=\"http://musicbrainz.org/ns/ext#-2.0\"><artist-list count=\"2\" offset=\"0\"><artist id=\"dcaa4f81-bfb7-44eb-8594-4e74f004b6e4\" type=\"Group\" ext:score=\"100\"><name>NOFX</name><sort-name>NOFX</sort-name><country>US</country><area id=\"489ce91b-6658-3307-9877-795b68554c98\"><name>United States</name><sort-name>United States</sort-name></area><begin-area id=\"1f40c6e1-47ba-4e35-996f-fe6ee5840e62\"><name>Los Angeles</name><sort-name>Los Angeles</sort-name></begin-area><life-span><begin>1983</begin><ended>false</ended></life-span><alias-list><alias sort-name=\"No FX\">No FX</alias></alias-list><tag-list><tag count=\"1\"><name>rock</name></tag><tag count=\"3\"><name>punk</name></tag><tag count=\"3\"><name>american</name></tag><tag count=\"2\"><name>punk rock</name></tag><tag count=\"1\"><name>ska punk</name></tag><tag count=\"2\"><name>usa</name></tag><tag count=\"1\"><name>hardcore punk</name></tag><tag count=\"1\"><name>américain</name></tag><tag count=\"1\"><name>pop punk</name></tag><tag count=\"2\"><name>skate punk</name></tag><tag count=\"1\"><name>los angeles</name></tag><tag count=\"1\"><name>bay area</name></tag><tag count=\"1\"><name>classic pop punk</name></tag><tag count=\"1\"><name>california punk</name></tag><tag count=\"1\"><name>classic hardcore punk</name></tag><tag count=\"1\"><name>classic punk</name></tag><tag count=\"1\"><name>los angeles punk</name></tag><tag count=\"1\"><name>bay area punk</name></tag></tag-list></artist><artist id=\"8ab0d990-40d2-4625-835d-c68a16295ee0\" type=\"Person\" ext:score=\"72\"><name>DJ NoFx</name><sort-name>NoFx, DJ</sort-name><gender>male</gender><disambiguation>dance music DJ/producer Patrick Sanders</disambiguation><life-span><ended>false</ended></life-span><alias-list><alias sort-name=\"Sanders, Patrick\">Patrick Sanders</alias></alias-list></artist></artist-list></metadata>";
+        }
     }
-}
 }
