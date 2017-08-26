51c2a3c20575f2fe66ee73e54955972e88cd351c
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-08-26 18:19:35 +1000

    Added new service for dependency injection

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,8 @@ namespace musichino
             // Add framework services.
             services.AddMvc();
 
-            services.AddScoped<MusicbrainzService>();
+            services.AddScoped<MusicbrainzService>()
+                .AddScoped<MessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
