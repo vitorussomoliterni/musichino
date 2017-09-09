a5d3d12dfd11930a2d6b3f48c126925cbb6e164d
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-09 17:17:37 +1000

    Trying to implement enum

--- src/Services/MessageService.cs
+++ src/Services/MessageService.cs
@@ -19,5 +19,39 @@ namespace musichino.Services
                 throw ex;
             }
         }
+
+        public Commands GetMessageCommand(string message)
+        {
+            try
+            {
+                var messageDeserialiser = new { update_id = "", message = new MessageModel() };
+                var messageBody = JsonConvert.DeserializeAnonymousType(message, messageDeserialiser);
+                return GetCommandType(messageBody.message.Text);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Commands GetCommandType(string text)
+        {
+            var indexOfFirstSpace = text.IndexOf(" ");
+            var command = text.Substring(0, indexOfFirstSpace + 1).ToLower();
+
+            if (command.Equals("add"))
+            {
+                return Commands.SearchArtist;
+            }
+            
+            throw new InvalidOperationException();
+        }
+
+        public enum Commands 
+        {
+            SearchArtist,
+            AddArtist,
+            RemoveArtist
+        }
     }
 }
