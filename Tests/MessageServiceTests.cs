5e1a3fafe3e71805f7769e5dfa85b718440c140f
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-09 17:51:40 +1000

    Fixed issue with json

--- Tests/MessageServiceTests.cs
+++ Tests/MessageServiceTests.cs
@@ -13,18 +13,18 @@ namespace Tests
     {
         private string MessageFactory(string text)
         {
-            var message = "'{\"update_id\":10000,\"message\":{\"date\":1441645532,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + text + "\"}}'";
+            var message = "{\"update_id\":10000,\"message\":{\"date\":1441645532,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + text + "\"}}";
             return message;
         }
         [Fact]
         public void TestGetMessageCommand_ShouldReturnAddArtist()
         {
             var service = new MessageService();
-            var message = MessageFactory("/add nofx");
+            var message = MessageFactory("add nofx");
 
             var result = service.GetMessageCommand(message);
 
-            Assert.Equal(Commands.SearchArtist, result);
+            Assert.Equal(Commands.Add, result);
         }
     }
 }
