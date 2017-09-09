5a65a5b0ff10935481acb39224856117e01e83dd
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-09 17:18:01 +1000

    Adapting test to new enum decision

---
+++ Tests/MessageServiceTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using musichino.Models;
+using musichino.Services;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using static musichino.Services.MessageService;
+
+namespace Tests
+{
+    public class MessageServiceTests
+    {
+        private string MessageFactory(string text)
+        {
+            var message = "'{\"update_id\":10000,\"message\":{\"date\":1441645532,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + text + "\"}}'";
+            return message;
+        }
+        [Fact]
+        public void TestGetMessageCommand_ShouldReturnAddArtist()
+        {
+            var service = new MessageService();
+            var message = MessageFactory("/add nofx");
+
+            var result = service.GetMessageCommand(message);
+
+            Assert.Equal(Commands.SearchArtist, result);
+        }
+    }
+}
