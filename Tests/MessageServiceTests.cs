6b35ea49dba2c9285e9f34809064eb360253ffc2
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-16 16:39:02 +1000

    Edited test to reflect new logic

--- Tests/MessageServiceTests.cs
+++ Tests/MessageServiceTests.cs
@@ -5,26 +5,76 @@ using musichino.Services;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text;
-using static musichino.Services.MessageService;
+using System.IO;
 
 namespace Tests
 {
     public class MessageServiceTests
     {
-        private string MessageFactory(string text)
+        private string rawMessageFactory(string userMessage)
         {
-            var message = "{\"update_id\":10000,\"message\":{\"date\":1441645532,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + text + "\"}}";
-            return message;
+            var rawMessage = "{\"update_id\":10000,\"message\":{\"date\":1441645532,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + userMessage + "\"}}";
+            return rawMessage;
         }
+
         [Fact]
         public void TestGetMessageCommand_ShouldReturnAddArtist()
         {
+            //Given
             var service = new MessageService();
-            var message = MessageFactory("add nofx");
+            var rawMessage = rawMessageFactory("add nofx");
 
-            var result = service.GetMessageCommand(message);
+            //When
+            var actualText = service.GetMessageCommand(rawMessage);
+            var expectedText = MessageService.Commands.Add;
 
-            Assert.Equal(Commands.Add, result);
+            //Then
+            Assert.Equal(expectedText, actualText);
         }
+
+        [Fact]
+        public void TestGetMessage_ShouldGetTheMessageText()
+        {
+            //Given
+            var service = new MessageService();
+            var text = "add nofx";
+            var rawMessage = rawMessageFactory(text);
+
+            //When
+            var actualMessage = service.GetMessage(rawMessage);
+            var expectedMessage = new MessageModel()
+            {
+                MessageId = 10000,
+                LastName = "Test Lastname",
+                FirstName = "Test",
+                Username = "Test",
+                Text = text
+            };
+
+            //Then
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+
+        // TODO: Add testing for reading the request
+        /*
+        [Fact]
+        public async Task TestReadRequestBodyAsync_ShouldReturnString()
+        {
+            //Given
+            var service = new MessageService();
+            var message = messageFactory("add nofx");
+            var stream = new byte[255];
+            
+            //When
+            using (var writer = new StreamWriter(new MemoryStream(stream)))
+            {
+                writer.WriteLine(message);
+            }
+            var result = await service.ReadRequestBodyAsync(stream);
+            
+            //Then
+            Assert.Equal();
+        }
+        */
     }
 }
