66158a9eda68b96cbcae1c9ef487be13ccf2ff27
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-17 18:56:36 +1000

    Made json more readable. I'm sure it was a productive thing to do, somehow

--- Tests/MessageServiceTests.cs
+++ Tests/MessageServiceTests.cs
@@ -12,7 +12,25 @@ namespace Tests
     {
         private string rawMessageFactory(string userMessage)
         {
-            var rawMessage = "{\"update_id\":10000,\"message\":{\"date\":1483228800,\"chat\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"message_id\":1365,\"from\":{\"last_name\":\"Test Lastname\",\"id\":1111111,\"first_name\":\"Test\",\"username\":\"Test\"},\"text\":\"" + userMessage + "\"}}";
+            var rawMessage = @"{'update_id': 10000,
+                                'message': {
+                                    'date': 1483228800,
+                                       'chat': {
+                                            'last_name': 'Test Lastname',
+                                            'id': 1111111,
+                                            'first_name': 'Test',
+                                            'username': 'Test'
+                                            },
+                                        'message_id': 1365,
+                                        'from': {
+                                            'last_name': 'Test Lastname',
+                                            'id': 1111111,
+                                            'first_name': 'Test',
+                                            'username': 'Test'
+                                            },
+                                        'text':'" + userMessage + 
+                                    @"'}
+                                }";
             return rawMessage;
         }
 
