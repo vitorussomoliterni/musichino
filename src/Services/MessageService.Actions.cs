ccba502a161477422f4026fb1563963cc246adfd
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-23 10:45:40 +1000

    Starting to build a partial class for the actions

---
+++ src/Services/MessageService.Actions.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace musichino.Services
+{
+    public partial class MessageService
+    {
+        public void Actionify(Commands action)
+        {
+            switch (action)
+            {
+                case Commands.Add:
+                    // Let's add
+                    break;
+                case Commands.Help:
+                    // Let's help
+                    break;
+                case Commands.List:
+                    // Let's list
+                    break;
+                case Commands.Other:
+                    // Let's other
+                    break;
+                case Commands.Remove:
+                    // Let's remove
+                case Commands.Suspend:
+                    // Let's suspend
+                default:
+                    // Why are you even here
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
