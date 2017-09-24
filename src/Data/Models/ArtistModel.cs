8cc0beb4e08d6b8bef3fde1644790182f8a2254f
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-09-24 15:18:56 +1000

    Adding properties

--- src/Data/Models/ArtistModel.cs
+++ src/Data/Models/ArtistModel.cs
@@ -1,13 +1,29 @@
+using System;
+using System.Collections.Generic;
+
 namespace musichino.Data.Models
 {
     public class ArtistModel
     {
-        public string Id { get; set; }
+        public ArtistModel()
+        {
+            Users = new List<UserModel>();
+        }
+        public Guid Id { get; set; }
+        public int ExternalId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Country { get; set; }
         public string BeginYear { get; set; }
         public string EndYear { get; set; }
         public bool? Ended { get; set; }
+        public List<UserModel> Users { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime LastCheckedAtUtc { get; set; }
+        public string LatestReleaseName { get; set; }
+        public string LatestReleaseExternalId { get; set; }
+        public DateTime LatestReleaseDateUtc { get; set; }
+        public DateTime ModifiedAtUtc { get; set; }
+        public DateTime DeletedAtUtc { get; set; }
     }
 }
