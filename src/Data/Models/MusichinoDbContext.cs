7063eb26f66b89c6c49d16462db2755d5ff479f5
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-11-03 19:21:36 +1100

    Added join table entity

--- src/Data/Models/MusichinoDbContext.cs
+++ src/Data/Models/MusichinoDbContext.cs
@@ -9,8 +9,9 @@ namespace musichino.Data.Models
         public MusichinoDbContext(DbContextOptions<MusichinoDbContext> options)
             : base(options)
         { }
-        public DbSet<ArtistModel> Astists { get; set; }
+        public DbSet<ArtistModel> Artists { get; set; }
         public DbSet<UserModel> Users { get; set; }
         public DbSet<ActionModel> Actions { get; set; }
+        public DbSet<ArtistUserModel> ArtistUsers { get; set; }
     }
 }
